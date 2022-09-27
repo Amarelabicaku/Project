67e934b7dc0b54c2bf5ad0406a7a0c70bf4cde81
Amarela <amarelabicaku@gmail.com>
2022-09-27 10:23:22 -0700

    Initial commit

---
+++ src/main/java/model/Alien.java
@@ -0,0 +1,25 @@
+package com.example.restapitelusko;
+
+public class Alien {
+    private String name;
+    private int points;
+
+    public Alien() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}
